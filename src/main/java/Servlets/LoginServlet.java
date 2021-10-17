12f4afeea75439ca27b9701af15602ffb5f3f873
Evgenii <Evgenii@bdeb>
2021-10-17 06:39:39 -0400

    restore LoginServlet

--- src/main/java/Servlets/LoginServlet.java
+++ src/main/java/Servlets/LoginServlet.java
@@ -1,11 +1,14 @@
 package Servlets;
 
+import dao.PersonneDao;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", value = "/LoginServlet")
@@ -16,8 +19,14 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getSession().setAttribute("idPersonne", 3);
-        System.out.println(request.getRequestURL());
+        int id = Integer.parseInt(request.getParameter("uname"));
+        String password = request.getParameter("psw");
+
+        int myid = PersonneDao.findOne(id, password);
+        if (myid != -1) {
+            HttpSession session = request.getSession();
+            session.setAttribute("idPersonne", id);
+        }
         RequestDispatcher disp = getServletContext().getRequestDispatcher("/index.jsp");
         disp.forward(request, response);
     }
